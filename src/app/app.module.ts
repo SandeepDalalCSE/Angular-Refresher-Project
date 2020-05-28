1691db232f825bce755f88e78440422ac14d7a26
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-28 23:22:20 +0530

    Handling User Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 // Registering persons component in app module.
 import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
   // the selector of persons component will look in all other component in the declarations.
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
