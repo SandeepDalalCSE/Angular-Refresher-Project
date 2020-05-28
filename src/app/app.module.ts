c65572378874e75d5a35c74855dbfd06e68ad034
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-28 22:53:14 +0530

    Creating Our First Component and Registering it in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Registering persons component in app module.
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
+  // the selector of persons component will look in all other component in the declarations.
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html'
+})
+export class PersonsComponent {
+
+}
+
+
+// After creating we need to register this component in out angular module or some other module.
