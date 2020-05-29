215aeaf668d2c7dd7c267c25534ea3d3b0cdf521
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-29 07:42:11 +0530

    Working with Angular Lifecycle Hooks

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,15 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent {
+export class PersonsComponent implements OnInit {
   personList: string[] = [];
 
-  constructor(private prsService: PersonsService) {
-    this.personList = prsService.persons;
+  constructor(private prsService: PersonsService) { // Note prsService is the property of PersonsComponent Class and also instance of PersonsService Class.
+    // this.personList = prsService.persons;  // this is not recommended way
+  }
+
+  ngOnInit() {
+    this.personList = this.prsService.persons;
   }
   // @Input() personList: string[] = [];
 }
