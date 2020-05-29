af133ab5e183085db3170134bcdf3727cdbf0737
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-29 08:30:30 +0530

    "Push"ing Data Around with Subjects

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[] = [];
+  private personListSubcription: Subscription;
 
   constructor(private prsService: PersonsService) { // Note prsService is the property of PersonsComponent Class and also instance of PersonsService Class.
     // this.personList = prsService.persons;  // this is not recommended way
@@ -14,10 +16,20 @@ export class PersonsComponent implements OnInit {
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSubcription = this.prsService.personChanged.subscribe(persons => {
+      console.log(persons);
+      this.personList = persons;
+    });
   }
 
   onRemovePerson(personName: string) {
     this.prsService.removePerson(personName);
+  }
+
+  ngOnDestroy() {
+    if (this.personListSubcription) {
+      this.personListSubcription.unsubscribe();
+    }
   }
   // @Input() personList: string[] = [];
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PersonsService {
+  personChanged = new Subject<string[]>();
   persons: string[] = ['Sandeep', 'Ajay', 'Bhavuk'];
 
   addPerson(name: string) {
-    this.persons.push(name);
+    this.persons.push(name); // item is push into array but still not updated to the DOM because of angular behavior.
+    this.personChanged.next(this.persons); // Here we updating fresh persons array using the object of Subject.
     console.log(this.persons);
   }
 
@@ -17,5 +20,8 @@ export class PersonsService {
       return person !== name;
     });
     console.log(this.persons);
+    this.personChanged.next(this.persons);
   }
+
+  // We need to subscribe this event 'personChanged' in other parts of the app.
 }
